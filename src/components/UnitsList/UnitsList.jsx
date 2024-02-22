c90189a8675705c9f0743b7c786a80bb22919fce
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-02-22 19:50:31 +0000

    renamed pageoutpage

--- src/components/UnitsList/UnitsList.jsx
+++ src/components/UnitsList/UnitsList.jsx
@@ -2,26 +2,26 @@ import { Link, Outlet } from "react-router-dom";
 import placeholderImg from "../../assets/img_placeholder.png";
 import "./UnitsList.css"
 
-export default function PageOutPage() {
-    return <div className="PageOutPage">
-        <h2 className="PageOutPage_header">
+export default function UnitsList() {
+    return <div className="UnitsList">
+        <h2 className="UnitsList_header">
             PAGE OUT RESOURCES
         </h2>
         {/* Link to="unitslist" className="MainPage_pageOutSelector" */}
-        <div className="PageOutPage_unitSelector">
-            <Link to="K9" className="PageOutPage_UnitSelectionButton">
+        <div className="UnitsList_unitSelector">
+            <Link to="K9" className="UnitsList_UnitSelectionButton">
                 <img alt="K9 logo" src={placeholderImg}></img>
                 <h2>K9</h2>
             </Link>
-            <Link to="ERT" className="PageOutPage_UnitSelectionButton">
+            <Link to="ERT" className="UnitsList_UnitSelectionButton">
                 <img alt="K9 logo" src={placeholderImg}></img>
                 <h2>ERT</h2>
             </Link>
-            <Link to="CIB" className="PageOutPage_UnitSelectionButton">
+            <Link to="CIB" className="UnitsList_UnitSelectionButton">
                 <img alt="K9 logo" src={placeholderImg}></img>
                 <h2>CIB</h2>
             </Link>
-            <Link to="CIC" className="PageOutPage_UnitSelectionButton">
+            <Link to="CIC" className="UnitsList_UnitSelectionButton">
                 <img alt="K9 logo" src={placeholderImg}></img>
                 <h2>CIC</h2>
             </Link>
