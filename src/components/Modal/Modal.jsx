2404ca189c8435c2127df3b8bddad99ef13bd8ac
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-03-21 17:55:47 +0000

    minimize modal by clicking outside of it

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import "./Modal.css"
 import EditK9Unit from '../EditUnit/EditK9Unit';
+import { useDispatch } from 'react-redux';
+import { setFormModalState, updateFormData } from '../../store';
 
 export default function Modal(props) {
   if(!props.openStateProp) return null;
+
+  const dispatch = useDispatch();
+
+  function handleClose() {
+    dispatch(setFormModalState(false));
+    dispatch(updateFormData({}));
+  }
+
   return ReactDOM.createPortal(
     <>
-    <div className="modal_overlay"></div>
+    <div className="modal_overlay" onClick={handleClose}></div>
     <div className="modal_div">
       <EditK9Unit />
     </div>
